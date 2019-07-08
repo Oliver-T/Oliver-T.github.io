2da8891c1b4106bde7e6aa157281fa58a0626ecd
Oliver Trad <olivertrad@gmail.com>
2019-07-08 14:32:45 +1000

    opera
    operapedia page added

--- js/main.js
+++ js/main.js
@@ -17,3 +17,29 @@ $(window).on('scroll', function () {
   });
 });
 
+$(document).ready(function() {
+ 
+  $(".opera-owl").owlCarousel({
+ 
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+ 
+  });
+ 
+});
